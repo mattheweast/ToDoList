8d76142e72526d45d173c60249c719faff30b14c
Matthew East <account.matteast@icloud.com>
2025-07-22 13:09:15 +0100

    Project Setup

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Work Mac on 22/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
